4f1104d6e99b9d3fece57540526b75dfe1ca5205
Gongwasubash <subashgongwanepal@gmail.com>
2025-09-30 23:53:53 +0545

    Add auto superuser creation

---
+++ schoolmgmt/management/commands/createsu.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+import os
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@school.com', 'admin123')
+            self.stdout.write('Superuser created: admin/admin123')
+        else:
+            self.stdout.write('Superuser already exists')
