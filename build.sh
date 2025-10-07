89b17e614f643f0960a8e94fdc5026aed8a11f87
Gongwasubash <subashgongwanepal@gmail.com>
2025-10-07 17:13:42 +0545

    Add build script for deployment migrations

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-python manage.py collectstatic --noinput
-python manage.py makemigrations
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsu
